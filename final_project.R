d809e0b68f05c4bbc82f972f214ac6938ce0431c
kyndall-brown <100884388+kyndall-brown@users.noreply.github.com>
2022-11-22 17:23:14 -0600

    Add files via upload

--- final_project.R
+++ final_project.R
@@ -1,6 +1,8 @@
 # final project - map of inflation across the U.S. States
 library(tidyverse)
 library(lubridate)
+library(gganimate)
+library(usmap)
 
 # import the data: it's all from FRED
 # national inflation data
@@ -42,8 +44,49 @@ for (i in 1:50) {
 }
 
 
+######## test code, doesn't really work yet #########
+# Animating personal income over time
+p = ggplot()
+for (i in 1:10) {
+  p = p + geom_line(data = statesmapdf, aes(x = observation_date,
+                                            y = state.name[i]), 
+                    color = )
+}
 
+#usmap
+library(usmap)
+plot_usmap(regions = "states", labels = TRUE) +
+  labs(title = "U.S. States",
+       subtitle = "We will create a heatmap.") +
+  theme(panel.background=element_blank()) 
 
+library(usmap)
+library(ggplot2)
+# create the dataframe to plot the year 2021
+# plot_usmap requires a dataframe made of 2 columns, one called "state"
+# state must have the abbreviations of the states
+st <- data.frame (
+  state = c("AL", "AK", "AZ", "AR", "CA", "CO", 
+           "CT","DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", 
+           "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", 
+           "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", 
+           "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", 
+           "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"),
+  personal_inc = c("")
+)
+# fill personal_inc with the personal income values of the states
+for (i in 1:50) {
+  st[["personal_inc"]][[i]] <- 
+    # "state_name" must match state #i in state.name and the year we need
+    statesmapdf["personal_income"][statesmapdf["state_name"]==state.name[i] 
+            & statesmapdf["observation_date"]=="2021-01-01"]
+}
+# i recommend reading the documentation about usmap, it is pretty helpful:
+# https://www.rdocumentation.org/packages/usmap/versions/0.6.1/topics/plot_usmap
+plot_usmap(data = st, values = "personal_inc", color = "red") +
+  scale_fill_continuous(low = "white", high = "red", 
+                        name = "personal income") + 
+  theme(legend.position = "right")
 
 
 
